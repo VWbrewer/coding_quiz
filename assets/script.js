355c8fedd092e9c30b56f748cc8ec8f0f529bff4
cbrewer <144485839+VWbrewer@users.noreply.github.com>
2023-10-11 10:08:10 -0500

    added code to the README

--- assets/script.js
+++ assets/script.js
@@ -27,3 +27,31 @@ function countdown() {
       }
     }, 1000);
   }
+
+
+
+
+
+
+
+
+  var myQuestions = [
+	{
+		question: "What is 10/2?",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '115'
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "What is 30/3?",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '10'
+		},
+		correctAnswer: 'c'
+	}
+];
